d408b340214c30c3e76c138ec6d90cba7eb2836b
Ibrahim7Younis <ibrahim7younis@gmail.com>
2025-06-01 23:16:47 +0300

    added DragAndDrop Test

--- src/test/java/wdioDemoTest/DragAndDropTest.java
+++ src/test/java/wdioDemoTest/DragAndDropTest.java
@@ -1,5 +1,8 @@
 package wdioDemoTest;
 
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import org.openqa.selenium.DeviceRotation;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,6 +17,12 @@ public class DragAndDropTest extends BaseTest{
     }
     @Test
     public void DragAndDropAllElements(){
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+        driver.pressKey(new KeyEvent(AndroidKey.HOME));
+        driver.pressKey(new KeyEvent(AndroidKey.ENTER));
+
+        driver.setClipboardText("Hello");
+        String text = driver.getClipboardText();
         dragAndDropPage.dragAndDropAll();
         Assert.assertTrue(dragAndDropPage.isCongratsMessageDisplayed(), "Not all elements were dragged and dropped");
     }
